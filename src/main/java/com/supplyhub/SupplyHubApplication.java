9624394fb8b8a57eff981941e5d6210beb8a03ce
jamillalobo <lobo.jamilla@gmail.com>
2025-03-30 15:01:57 -0300

    initial commit

---
+++ src/main/java/com/supplyhub/SupplyHubApplication.java
@@ -0,0 +1,13 @@
+package com.supplyhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplyHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supplyhub/SupplyHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supplyhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
